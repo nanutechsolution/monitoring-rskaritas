ef96dfb717ace946f3167b695b9bd4a28e9ad3ec
nanu <ranusate19@gmail.com>
2025-10-26 08:19:43 +0800

    update

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -83,7 +83,7 @@ $logout = function (Logout $logout) {
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                {{ __('Dashboard') }}
+                {{ __('Kamar Inap') }}
             </x-responsive-nav-link>
         </div>
 
